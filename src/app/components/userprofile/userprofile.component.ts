aa97087ad89a8fbbef5fb1fd366762fafdc8865a
KubanD936 <kuban627@revature.net>
2022-12-13 16:37:24 -0500

    Worked on Userprofile comp, need to work on wiring

--- src/app/components/userprofile/userprofile.component.ts
+++ src/app/components/userprofile/userprofile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AccountService } from 'src/app/services/account.service';
+
 
 @Component({
   selector: 'app-userprofile',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserprofileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AccountService: AccountService, private router: Router) { }
+
+  currentAccount: User = { //waiting for Sergio to update User model on frontend
+    id: 0,
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    address: '',
+    phoneNumber: '',
+  };
 
   ngOnInit(): void {
+    this.AccountService.getAccount().subscribe();
   }
 
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,10 +2,30 @@ export class User {
     id: number;
     email: string;
     password: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    dateOfBirth: string;
+    address: string;
 
-    constructor(_id: number, _email: string, _password: string) {
+    constructor(
+        _id: number,
+        _email: string,
+        _password: string,
+        _firstName: string,
+        _lastName: string,
+        _phoneNumber: string,
+        _dateOfBirth: string,
+        _address: string
+      ) {
         this.id = _id;
         this.email = _email;
         this.password = _password;
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+        this.phoneNumber = _phoneNumber;
+        this.dateOfBirth = _dateOfBirth;
+        this.address = _address;
+
     }
 }
