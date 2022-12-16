292c48ea4c314db535bca7610a9d8cf6250c9b34
Sergio H. Passos <48944090+SergioHPassos@users.noreply.github.com>
2022-12-16 13:37:05 -0500

    updated header

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ export const environment = {
   url: 'http://aloanbanksapp-env.eba-yuubmsje.us-east-1.elasticbeanstalk.com/',
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Origin':
+      'http://aloanbanks-frontend-bucket.s3-website-us-east-1.amazonaws.com',
     'Current-User': '',
   },
   withCredentials: true,
