b110a999d5dc3f69113ad5108f945c2cfc936b78
Sergio H. Passos <48944090+SergioHPassos@users.noreply.github.com>
2022-12-21 20:44:18 -0500

    updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://aloanbanksapp-env.eba-yuubmsje.us-east-1.elasticbeanstalk.com/',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin':
+      'http://aloanbanks-frontend-bucket.s3-website-us-east-1.amazonaws.com',
+    'Current-User': '',
+  },
+  withCredentials: true,
 };
